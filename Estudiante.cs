dd805bd51dbbf98f0b3a505298995d2b77fb8e62
Josue <jjchaves@edu.upolitecnica.cr>
2024-09-23 21:15:06 -0600

    Primer Tarea

---
+++ Estudiante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_estudiantes
+{
+    internal class Estudiante
+    {
+        public string Nombre;
+        private int Edad;
+        public double Promedio;
+        Estudiante (string nombre, int edad, double promedio)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Promedio = promedio;
+        }
+
+    public bool mayor()
+    {
+        return Edad >= 18;
+    }
+
+    public void info()
+    {
+        Console.WriteLine("Nombre: {Nombre}, Promedio: {Promedio}");
+    }
+}
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_estudiantes
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Estudiante> estudiantes = new List<Estudiante>();
+
+            Console.WriteLine("Por favor, ingrese la cantidad de estudiantes a registrar: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Por favor, ahora ingrese los datos del estudiante {i + 1}");
+                Console.WriteLine("Nombre: ");
+                string nombre = Console.ReadLine();
+
+                Console.WriteLine("Edad: ");
+                int edad = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Promedio: ");
+                double promedio = double.Parse(Console.ReadLine());
+
+                Estudiante estudiante = new Estudiante(nombre,
+                                                       edad, 
+                                                       promedio);
+
+                estudiantes.Add(estudiante);
+
+            }
+
+            Console.WriteLine("Los estudiantes cin promedio mayor o igual a 70");
+            int index = 0;
+            while (index < estudiantes.Count)
+            {
+                if (estudiantes[index].Promedio >= 70)
+                {
+                    estudiantes[index].info();
+
+                    if (estudiantes[index].mayor())
+                    {
+                        Console.WriteLine("{estudiantes[i].Nombre} es mayor de edad");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("{estudiantes[i].Nombre} no es mayor de edad");
+                    }
+                }
+                index++;
+                Console.ReadKey();
+            }
+        }
+    }
+}
